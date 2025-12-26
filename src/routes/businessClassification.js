f62a9fbd3eeeb62783b36137abbe291b3e183eb5
Vipulkoli2000 <Vipulkoli2323@gmail.com>
2025-12-26 16:50:34 +0530

    chnages

--- src/routes/businessClassification.js
+++ src/routes/businessClassification.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { searchClientEdge, getOrderFieldsGrouped } = require("../controllers/businessClassificationController");
+const { searchClientEdge, searchBookingMatrix, searchCounterPartyMatrix, getOrderFieldsGrouped } = require("../controllers/businessClassificationController");
 
 const router = express.Router();
 
 // GET /api/business-classification/client-edge
 router.get("/client-edge", searchClientEdge);
+router.get("/booking-matrix", searchBookingMatrix);
+router.get("/counterparty-matrix", searchCounterPartyMatrix);
 
 // GET /api/business-classification/order-fields
 router.get("/order-fields", getOrderFieldsGrouped);
